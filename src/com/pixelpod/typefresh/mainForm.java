961c66c6f1f2b0099a73855fc7fb4ac6df9856e7
Timothy Caraballo <openback@opendesk.(none)>
2010-03-04 18:41:16 -0500

    Added README

--- src/com/pixelpod/typefresh/mainForm.java
+++ src/com/pixelpod/typefresh/mainForm.java
@@ -25,7 +25,6 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
 import android.widget.ListView;
 import android.widget.Toast;
 
